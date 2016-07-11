aaa67380d05145897615d0017292d1012fdb7561
Andriy Chaika <andriy@chaika.in.ua>
2016-07-11 13:58:14 +0300

    Fix document.body check

--- cocos2d/core/labelttf/CCLabelTTF.js
+++ cocos2d/core/labelttf/CCLabelTTF.js
@@ -825,12 +825,9 @@ cc.LabelTTF.__labelHeightDiv.style.left = "-100px";
 cc.LabelTTF.__labelHeightDiv.style.top = "-100px";
 cc.LabelTTF.__labelHeightDiv.style.lineHeight = "normal";
 
-document.body ?
-    document.body.appendChild(cc.LabelTTF.__labelHeightDiv) :
-    window.addEventListener('load', function () {
-        this.removeEventListener('load', arguments.callee, false);
-        document.body.appendChild(cc.LabelTTF.__labelHeightDiv);
-    }, false);
+cc.onBodyReady(function (){
+    document.body.appendChild(cc.LabelTTF.__labelHeightDiv);
+});
 
 cc.LabelTTF.__getFontHeightByDiv = function (fontName, fontSize) {
 
